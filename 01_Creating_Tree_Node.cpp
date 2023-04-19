29837be8a080a1867cde745289658c714f9539ba
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2023-04-19 22:25:36 +0530

    Create 01_Creating_Tree_Node.cpp

---
+++ 01_Creating_Tree_Node.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+template<typename T>
+class TreeNode{
+    public:
+        T data;
+        vector<TreeNode<T>*> childern;
+        TreeNode(T data){
+            this->data=data;
+        }
+};
+int main(){
+    //Creating Root Node
+    TreeNode<int> * root = new TreeNode<int>(1);
+    //Creating Child
+    TreeNode<int> * n1 = new TreeNode<int>(2);
+    TreeNode<int> * n1 = new TreeNode<int>(1);
+    //Linking Children
+    root->children.push_back(n1);
+    root->children.push_back(n2);
+    return 0;
+}
+/*
+Here n1 & n2 are leaf node (No Children)
+n1 & n2 are siblings
+root node has descendants of n1 & n2
+
+*/
