bd6d3972f1e6c7eb9e82f1c202e64ce6ccff103a
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2023-04-19 22:33:40 +0530

    Update 01_Creating_Tree_Node.cpp

--- 01_Creating_Tree_Node.cpp
+++ 01_Creating_Tree_Node.cpp
@@ -15,7 +15,7 @@ int main(){
     TreeNode<int> * root = new TreeNode<int>(1);
     //Creating Child
     TreeNode<int> * n1 = new TreeNode<int>(2);
-    TreeNode<int> * n1 = new TreeNode<int>(1);
+    TreeNode<int> * n2 = new TreeNode<int>(1);
     //Linking Children
     root->children.push_back(n1);
     root->children.push_back(n2);
