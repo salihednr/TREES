73d20b2912a1b19e98c4786726d28908723312e7
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2023-04-19 22:37:10 +0530

    Create 02_Print_Tree_Recursively.cpp

---
+++ 02_Print_Tree_Recursively.cpp
@@ -0,0 +1,40 @@
+/*
+Simple Way To Print Tree
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+template<typename T>
+class TreeNode{
+    public:
+        T data;
+        vector<TreeNode<T>*> children;
+        TreeNode(T data){
+            this->data=data;
+        }
+};
+//Print Recursively
+void printRec(TreeNode<int> *root){
+    cout<<root->data<<endl;
+    for(int i=0;i<root->children.size();i++){
+        printRec(root->children[i]);
+    }
+}
+int main(){
+    //Creating Root Node
+    TreeNode<int> * root = new TreeNode<int>(1);
+    //Creating Child
+    TreeNode<int> * n1 = new TreeNode<int>(2);
+    TreeNode<int> * n2 = new TreeNode<int>(3);
+    //Linking Children
+    root->children.push_back(n1);
+    root->children.push_back(n2);
+    //Printing
+    printRec(root);
+    return 0;
+}
+/*
+Here n1 & n2 are leaf node (No Children)
+n1 & n2 are siblings
+root node has descendants of n1 & n2
+*/
