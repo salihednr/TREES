81cc388658b5c7cb03643a0790b6fef501deb38c
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2023-04-19 23:01:12 +0530

    Create 04_Tree_Input_Rec.cpp

---
+++ 04_Tree_Input_Rec.cpp
@@ -0,0 +1,54 @@
+/*
+Input Tree Using Recursively
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+template<typename T>
+class TreeNode{
+    public:
+        T data;
+        vector<TreeNode<T>*> children;
+        TreeNode(T data){
+            this->data=data;
+        }
+};
+//Print Recursively
+void printRec(TreeNode<int> *root){
+    cout<<root->data<<": ";
+    for(int i=0;i<root->children.size();i++)
+        cout<<root->children[i]->data<<" ";
+    cout<<endl;
+    for(int i=0;i<root->children.size();i++){
+        printRec(root->children[i]);
+    }
+}
+//Take Input Recursively
+TreeNode<int> *takeinput(){
+    //Root Data
+    int rootData;
+    cout<<"Enter Data: ";
+    cin>>rootData;
+    TreeNode<int> *root = new TreeNode<int>(rootData);
+    //Number Of Children
+    cout<<"Enter The Number Of Children Of "<<rootData<<" : ";
+    int num;
+    cin>>num;
+    for(int i=0;i<num;i++){
+        TreeNode<int>*child=takeinput();
+        root->children.push_back(child);
+    }
+    return root;
+}
+int main(){
+    TreeNode<int> * root = takeinput();
+    //Printing
+    printRec(root);
+    return 0;
+}
+/*
+Here n1 & n2 are leaf node (No Children)
+n1 & n2 are siblings
+root node has descendants of n1 & n2
+
+*/
